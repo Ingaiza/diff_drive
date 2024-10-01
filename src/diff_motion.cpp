137707c798667402961b07f4f90bcf219cb8dba0
Ingaiza <alvindavid898@gmail.com>
2024-10-01 15:53:55 +0300

    latching troubleshooting

--- src/diff_motion.cpp
+++ src/diff_motion.cpp
@@ -310,7 +310,11 @@ private:
             {
                 pwm_counter_a = 0;
             }
-            safe_gpio_write(Motor_A_EN, pwm_counter_a < duty_cycle_a ? 1 : 0);
+            while (right_duty_queue.size() == 1)
+            {
+                safe_gpio_write(Motor_A_EN, pwm_counter_a < duty_cycle_a ? 1 : 0);
+            }
+            
             RCLCPP_WARN(this->get_logger(),"MOTOR_A_EN PWM IS ACTIVE");
         }
         else
@@ -343,7 +347,11 @@ private:
             {
                 pwm_counter_b = 0;
             }
-            safe_gpio_write(Motor_B_EN, pwm_counter_b < duty_cycle_b ? 1 : 0);
+            while (left_duty_queue_size()<= 1)
+            {
+                safe_gpio_write(Motor_B_EN, pwm_counter_b < duty_cycle_b ? 1 : 0);
+            }
+            
             RCLCPP_WARN(this->get_logger(),"MOTOR_B_EN PWM IS ACTIVE");   
         }
         else
